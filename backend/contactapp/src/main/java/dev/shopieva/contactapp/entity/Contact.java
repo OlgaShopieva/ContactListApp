7ac08bb89749b8b97295f70506d0937f8ee15fcf
OlgaShopieva <shopieva94@gmail.com>
2023-09-01 22:02:42 +0300

    added Contact entity

--- backend/contactapp/src/main/java/dev/shopieva/contactapp/entity/Contact.java
+++ backend/contactapp/src/main/java/dev/shopieva/contactapp/entity/Contact.java
@@ -1,6 +1,31 @@
 package dev.shopieva.contactapp.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "contacts")
 public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    
+    @Column(name = "full_Name")
+    private String fullName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "notes")
+    private String notes;
     
     
 }
